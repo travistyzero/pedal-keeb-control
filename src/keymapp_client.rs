0a52e9234f4d57a3323e81a62abccf21b61f3d1d
Travis Bloomfield <travis@travisbloomfield.com>
2025-11-04 11:18:33 +1100

    Get connection to Keymapp working

--- src/keymapp_client.rs
+++ src/keymapp_client.rs
@@ -1,7 +1,14 @@
-use keymapp::keyboard_service_client::KeyboardServiceClient;
-use anyhow::Result;
-use tokio::sync::mpsc::Receiver;
 use crate::pedal::PedalPosition;
+use anyhow::{Context, Result};
+use keymapp::keyboard_service_client::KeyboardServiceClient;
+use std::path::Path;
+use hyper_util::rt::TokioIo;
+use tokio::net::UnixStream;
+use tokio::sync::mpsc::Receiver;
+use tonic::Request;
+use tonic::transport::{Channel, Endpoint, Uri};
+use tower::service_fn;
+use crate::keymapp_client::keymapp::{SetLayerRequest};
 
 pub mod keymapp {
     tonic::include_proto!("api");
@@ -9,14 +16,25 @@ pub mod keymapp {
 
 pub struct KeymappClient {
     rx: Receiver<PedalPosition>,
+    client: KeyboardServiceClient<Channel>,
 }
 
 impl KeymappClient {
     pub async fn new(rx: Receiver<PedalPosition>) -> Result<Self> {
-        // KeyboardServiceClient::connect("http://[::1]:50051").await?;
-        Ok(Self {
-            rx
-        })
+        let connector = service_fn(async |_: Uri| {
+            let stream = UnixStream::connect(Path::new("/home/travis/.config/.keymapp/keymapp.sock")).await?;
+            Ok::<TokioIo<UnixStream>, anyhow::Error>(TokioIo::new(stream))
+        });
+
+        let channel = Endpoint::try_from("http://[::]:50051")
+            .context("Invalid endpoint URL")?
+            .connect_with_connector(connector)
+            .await
+            .context("Failed to connect to keymapp server")?;
+
+        let client = KeyboardServiceClient::new(channel);
+
+        Ok(Self { rx, client })
     }
 
     pub async fn run(&mut self) -> Result<()> {
@@ -27,7 +45,17 @@ impl KeymappClient {
                     return Ok(());
                 }
                 Some(pedal_position) => {
-                    println!("Got position: {:?}", pedal_position);
+                    match pedal_position {
+                        PedalPosition::Down => {
+                            let req = Request::new(SetLayerRequest { layer: 2 });
+                            self.client.set_layer(req).await.context("Failed to set layer")?;
+                        }
+                        PedalPosition::Up => {
+                            let req = Request::new(SetLayerRequest{ layer: 2 });
+                            self.client.unset_layer(req).await.context("Failed to unset layer")?;
+                        }
+                    }
+
                 }
             };
         }
